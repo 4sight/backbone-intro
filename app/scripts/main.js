b62fc6ce30d749a239b6af6a89a9fa195155c179
Greeley Dawson <hgd4th@gmail.com>
2014-07-10 16:18:43 -0400

    Finished

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,3 @@
-var Song = Backbone.Model.extend({
-	defaults: {
-		extension: '.mp3'
-	},
-	initialize: function() {
-		var name = this.get('name');
-		console.log('RUN THA TRAAAAACK!!')
-	}
-});
-
-var Artist = Backbone.Model.extend({});
-var Album = Backbone.Model.extend({});
-
-var disorder = new Song({
-	name: 'Disorder',
-	artist: 'Joy Division'
-});
-
-disorder.get('name');
-
-Music = Backbone.Collection.extend ({
-	model: Song,
-	url: 'http://tiy-atl-fe-server.herokuapp.com/collections/greeley'
+music.fetch().done(function(){
+	new MusicView({collection: music});
 });
