aded46dbb2bb11d5792b178286f4b8433dce0319
Greeley Dawson <hgd4th@gmail.com>
2014-07-10 14:04:58 -0400

    Added collection

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,24 @@
-var Songs = Backbone.Model.extend({
+var Song = Backbone.Model.extend({
 	defaults: {
 		extension: '.mp3'
 	},
-
 	initialize: function() {
 		var name = this.get('name');
 		console.log('RUN THA TRAAAAACK!!')
 	}
 });
 
-var disorder = new Songs({
+var Artist = Backbone.Model.extend({});
+var Album = Backbone.Model.extend({});
+
+var disorder = new Song({
 	name: 'Disorder',
 	artist: 'Joy Division'
 });
 
 disorder.get('name');
+
+Music = Backbone.Collection.extend ({
+	model: Song,
+	url: 'http://tiy-atl-fe-server.herokuapp.com/collections/greeley'
+});
