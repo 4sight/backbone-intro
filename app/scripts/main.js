ce0096522267447256bc55a34905b0c5adc9482f
Greeley Dawson <hgd4th@gmail.com>
2014-07-10 13:41:35 -0400

    Set up model and created an instance

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log('The Iron Yard Rocks');
+var Songs = Backbone.Model.extend({
+	defaults: {
+		extension: '.mp3'
+	},
+
+	initialize: function() {
+		var name = this.get('name');
+		console.log('RUN THA TRAAAAACK!!')
+	}
+});
+
+var disorder = new Songs({
+	name: 'Disorder',
+	artist: 'Joy Division'
+});
+
+disorder.get('name');
