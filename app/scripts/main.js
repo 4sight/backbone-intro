9037bb322568114f0be779764addbcd3957cab6f
Greeley Dawson <hgd4th@gmail.com>
2014-07-14 13:09:19 -0400

    Handlebars installation attempt

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,9 @@
 music.fetch().done(function(){
 	new MusicView({collection: music});
 });
+
+var template = Handlebars.compile($('#student_list_h').html());
+var rendered = template({students: this.collection.toJSON()});
+console.log({students: this.collection.toJSON()});
+this.$el.html(rendered);
+return this;
